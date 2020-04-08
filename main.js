fe32a18a4f25d56b2ef1843ab734072ad913b40c
Yang Taehwan <taehwan920@gmail.com>
2020-04-08 20:04:46 +0900

    How to read data

--- main.js
+++ main.js
@@ -10,66 +10,74 @@ let app = http.createServer(function (request, response) {
 
     if (pathName === '/') {
         if (queryData.id === undefined) {
-            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-                queryData.id = 'welcome';
-                description = 'Hello, Node.js';
-                let template = `
-            <!doctype html>
-                <html>
-                    <head>
-                        <title>WEB1 - ${queryData.id}</title>
-                        <meta charset="utf-8">
-                    </head>
-                    <body>
-                        <h1><a href="/">WEB</a></h1>
-                        <ol>
-                            <li><a href="/?id=HTML">HTML</a></li>
-                            <li><a href="/?id=CSS">CSS</a></li>
-                            <li><a href="/?id=JavaScript">JavaScript</a></li>
-                        </ol>
-                        <h2>${queryData.id}</h2>
-                        <p>${description}</p>
-                    </body>
-                </html>
-            `;
+
+            fs.readdir('./data', function (error, fileList) {
+                console.log(fileList);
+                const title = 'welcome';
+                const description = 'Hello, Node.js';
+
+                var list = '<ul>';
+
+                for (let i = 0; i < fileList.length; i++) {
+                    list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`
+                }
+
+                list = list + '</ul>'
+
+                const template = `
+                    <!doctype html>
+                        <html>
+                            <head>
+                                <title>WEB1 - ${title}</title>
+                                <meta charset="utf-8">
+                            </head>
+                            <body>
+                                <h1><a href="/">WEB</a></h1>
+                                ${list}
+                                <h2>${title}</h2>
+                                <p>${description}</p>
+                            </body>
+                        </html>
+                `;
                 response.writeHead(200);
                 response.end(template);
-            });
+            })
         } else {
-            let title = queryData.id;
-            fs.readFile(`data/${title}`, 'utf8', function (err, description) {
-                let template = `
-            <!doctype html>
-                <html>
-                    <head>
-                        <title>WEB1 - ${title}</title>
-                        <meta charset="utf-8">
-                    </head>
-                    <body>
-                        <h1><a href="/">WEB</a></h1>
-                        <ol>
-                            <li><a href="/?id=HTML">HTML</a></li>
-                            <li><a href="/?id=CSS">CSS</a></li>
-                            <li><a href="/?id=JavaScript">JavaScript</a></li>
-                        </ol>
-                        <h2>${title}</h2>
-                        <p>${description}</p>
-                    </body>
-                </html>
-            `;
-                response.writeHead(200);
-                response.end(template);
+            fs.readdir('./data', function (error, fileList) {
+                var list = '<ul>';
+
+                for (let i = 0; i < fileList.length; i++) {
+                    list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`
+                }
+
+                list = list + '</ul>'
+
+
+                fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                    let title = queryData.id;
+                    let template = `
+                        <!doctype html>
+                            <html>
+                                <head>
+                                    <title>WEB1 - ${title}</title>
+                                    <meta charset="utf-8">
+                                </head>
+                                <body>
+                                    <h1><a href="/">WEB</a></h1>
+                                    ${list}
+                                    <h2>${title}</h2>
+                                    <p>${description}</p>
+                                </body>
+                            </html>
+                        `;
+                    response.writeHead(200);
+                    response.end(template);
+                });
             });
         }
     } else {
         response.writeHead(404);
         response.end('Not Found');
     }
-
-
-
-
-
-
 });
 app.listen(3000);
