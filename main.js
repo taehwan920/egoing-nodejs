c9c8415f543380b6f45cfdc442e1ddb280f09ef8
Yang Taehwan <taehwan920@gmail.com>
2020-04-08 16:14:25 +0900

    Complete dynamic page and title

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ let url = require('url');
 let app = http.createServer(function (request, response) {
     let _url = request.url;
     let queryData = url.parse(_url, true).query;
-    console.log(queryData.id);
+    let title = queryData.id;
     if (_url == '/') {
-        _url = '/index.html';
+        title = 'Welcome';
     }
     if (_url == '/favicon.ico') {
         response.writeHead(404);
@@ -15,7 +15,30 @@ let app = http.createServer(function (request, response) {
         return;
     }
     response.writeHead(200);
-    response.end(queryData.id);
+    fs.readFile(`data/${title}`, 'utf8', function (err, description) {
+        let template = `
+    <!doctype html>
+        <html>
+            <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1><a href="/">WEB</a></h1>
+                <ol>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                </ol>
+                <h2>${title}</h2>
+                <p>${description}</p>
+            </body>
+        </html>
+    `;
+        response.end(template);
+    });
+
+
 
 });
 app.listen(3000);
