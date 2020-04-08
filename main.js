34a56c4ffffea1af6d11d41fdf95374d87c0fc5f
Yang Taehwan <taehwan920@gmail.com>
2020-04-08 17:26:48 +0900

    Complete 404page and Home page

--- main.js
+++ main.js
@@ -5,38 +5,69 @@ let url = require('url');
 let app = http.createServer(function (request, response) {
     let _url = request.url;
     let queryData = url.parse(_url, true).query;
-    let title = queryData.id;
-    if (_url == '/') {
-        title = 'Welcome';
-    }
-    if (_url == '/favicon.ico') {
+    let pathName = url.parse(_url, true).pathname;
+
+
+    if (pathName === '/') {
+        if (queryData.id === undefined) {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                queryData.id = 'welcome';
+                description = 'Hello, Node.js';
+                let template = `
+            <!doctype html>
+                <html>
+                    <head>
+                        <title>WEB1 - ${queryData.id}</title>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1><a href="/">WEB</a></h1>
+                        <ol>
+                            <li><a href="/?id=HTML">HTML</a></li>
+                            <li><a href="/?id=CSS">CSS</a></li>
+                            <li><a href="/?id=JavaScript">JavaScript</a></li>
+                        </ol>
+                        <h2>${queryData.id}</h2>
+                        <p>${description}</p>
+                    </body>
+                </html>
+            `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        } else {
+            let title = queryData.id;
+            fs.readFile(`data/${title}`, 'utf8', function (err, description) {
+                let template = `
+            <!doctype html>
+                <html>
+                    <head>
+                        <title>WEB1 - ${title}</title>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1><a href="/">WEB</a></h1>
+                        <ol>
+                            <li><a href="/?id=HTML">HTML</a></li>
+                            <li><a href="/?id=CSS">CSS</a></li>
+                            <li><a href="/?id=JavaScript">JavaScript</a></li>
+                        </ol>
+                        <h2>${title}</h2>
+                        <p>${description}</p>
+                    </body>
+                </html>
+            `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
+    } else {
         response.writeHead(404);
-        response.end();
-        return;
+        response.end('Not Found');
     }
-    response.writeHead(200);
-    fs.readFile(`data/${title}`, 'utf8', function (err, description) {
-        let template = `
-    <!doctype html>
-        <html>
-            <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1><a href="/">WEB</a></h1>
-                <ol>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                </ol>
-                <h2>${title}</h2>
-                <p>${description}</p>
-            </body>
-        </html>
-    `;
-        response.end(template);
-    });
+
+
+
 
 
 
