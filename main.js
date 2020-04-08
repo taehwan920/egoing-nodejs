6d6b4f509d81632f041dc833dd0de484e289c3f0
Yang Taehwan <taehwan920@gmail.com>
2020-04-08 15:34:39 +0900

    What the query is

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-var http = require('http');
-var fs = require('fs');
-var app = http.createServer(function (request, response) {
-    var url = request.url;
-    if (request.url == '/') {
-        url = '/index.html';
+let http = require('http');
+let fs = require('fs');
+let url = require('url');
+
+let app = http.createServer(function (request, response) {
+    let _url = request.url;
+    let queryData = url.parse(_url, true).query;
+    console.log(queryData.id);
+    if (_url == '/') {
+        _url = '/index.html';
     }
-    if (request.url == '/favicon.ico') {
+    if (_url == '/favicon.ico') {
         response.writeHead(404);
         response.end();
         return;
     }
     response.writeHead(200);
-    console.log(__dirname + url)
-    response.end(fs.readFileSync(__dirname + url));
+    response.end(queryData.id);
 
 });
 app.listen(3000);
