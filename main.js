6d4e2016936731df11c2f7a806e3ecf579adce24
Yang Taehwan <taehwan920@gmail.com>
2020-04-09 18:15:28 +0900

    What the module is

--- main.js
+++ main.js
@@ -2,35 +2,8 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 const qs = require('querystring');
+const template = require('./lib/template');
 
-const template = {
-    html: (title, list, body, control) => {
-        return (
-            `<!doctype html>
-                <html>
-                    <head>
-                        <title>WEB1 - ${title}</title>
-                        <meta charset="utf-8">
-                    </head>
-                    <body>
-                        <h1><a href="/">WEB</a></h1>
-                        ${list}
-                        ${control}
-                        ${body}
-                    </body>
-                </html>
-            `
-        );
-    },
-    list: (fileList) => {
-        var list = '<ul>';
-        for (let i = 0; i < fileList.length; i++) {
-            list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`
-        }
-        list = list + '</ul>';
-        return list;
-    }
-}
 
 const templateList = template.list;
 const templateHTML = template.html;
