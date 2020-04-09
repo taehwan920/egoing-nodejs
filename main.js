4c2cb3b0701cc408759c35641939510826c883e1
Yang Taehwan <taehwan920@gmail.com>
2020-04-09 17:12:05 +0900

    Complete CRUD with Node.js!

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const url = require('url');
 const qs = require('querystring');
 
-const templateHTML = (title, list, body) => {
+const templateHTML = (title, list, body, control) => {
     return (
         `<!doctype html>
             <html>
@@ -14,7 +14,7 @@ const templateHTML = (title, list, body) => {
                 <body>
                     <h1><a href="/">WEB</a></h1>
                     ${list}
-                    <a href="/create">Create</a>
+                    ${control}
                     ${body}
                 </body>
             </html>
@@ -44,7 +44,9 @@ const app = http.createServer(function (request, response) {
                 const description = 'Hello, Node.js';
                 const template = templateHTML(title, list,
                     `<h2>${title}</h2>
-                    <p>${description}</p>`);
+                    <p>${description}</p>`,
+                    `<a href="/create">Create</a>`
+                );
 
                 response.writeHead(200);
                 response.end(template);
@@ -56,7 +58,14 @@ const app = http.createServer(function (request, response) {
                     const list = templateList(fileList);
                     const template = templateHTML(title, list,
                         `<h2>${title}</h2>
-                        <p>${description}</p>`);
+                        <p>${description}</p>`,
+                        `<a href="/create">Create</a> 
+                         <a href="/update?id=${title}">update</a>
+                         <form action="delete_process" method="post">
+                            <input type="hidden" name="id" value="${title}">
+                            <input type="submit" value="delete">
+                         </form>`
+                    );
 
                     response.writeHead(200);
                     response.end(template);
@@ -79,7 +88,7 @@ const app = http.createServer(function (request, response) {
                         <input type="submit">
                     </p>
                 </form>
-                `);
+                `, '');
             response.writeHead(200);
             response.end(template);
         })
@@ -96,6 +105,63 @@ const app = http.createServer(function (request, response) {
                 response.writeHead(302, { Location: `/?id=${title}` });
                 response.end('success');
             });
+        });
+    } else if (pathName === '/update') {
+        fs.readdir('./data', function (error, fileList) {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                const title = queryData.id;
+                const list = templateList(fileList);
+                const template = templateHTML(title, list,
+                    `
+                    <form action="/update_process" method="post">
+                        <input type="hidden" name="id" value="${title}">
+                        <p>
+                            <input type="text" name="title" placeholder="title" value="${title}">
+                        </p>
+                        <p>
+                            <textarea name="description" placeholder="description">${description}</textarea>
+                        </p>
+                        <p>
+                            <input type="submit">
+                        </p>
+                    </form>
+                    `,
+                    `<a href="/create">Create</a> <a href="/update?id=${title}">update</a>`
+                );
+
+                response.writeHead(200);
+                response.end(template);
+            });
+        });
+    } else if (pathName === '/update_process') {
+        let body = '';
+        request.on('data', function (data) {
+            body += data;
+        });
+        request.on('end', function () {
+            const post = qs.parse(body);
+            const id = post.id;
+            const title = post.title;
+            const description = post.description;
+            fs.rename(`data/${id}`, `data/${title}`, function (error) {
+                fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
+                    response.writeHead(302, { Location: `/?id=${title}` });
+                    response.end('success');
+                });
+            })
+        });
+    } else if (pathName === '/delete_process') {
+        let body = '';
+        request.on('data', function (data) {
+            body += data;
+        });
+        request.on('end', function () {
+            const post = qs.parse(body);
+            const id = post.id;
+            fs.unlink(`data/${id}`, function (error) {
+                response.writeHead(302, { Location: `/` });
+                response.end('success');
+            })
         });
     } else {
         response.writeHead(404);
